2a7b5fae0154e255ad93c3f9316008f71058929a
Aperçu <bgronon@gmail.com>
2014-03-20 01:58:16 +0100

    up

--- pagedown.js
+++ pagedown.js
@@ -3,11 +3,19 @@
 angular.module('pageDown').directive('pagedown', function ($timeout) {
 
 	return {
-		template: '<textarea class="wmd-input"></textarea>',
+		template: '<textarea></textarea>',
 		restrict: 'E',
 		link: function postLink(scope, element, attrs) {
-			console.log(attrs);
-			console.log(element);
+
+			var converter = Markdown.getSanitizingConverter();
+			if (attrs.sanitized == "false") {
+				converter = new Markdown.Converter();
+			}
+			if (attrs.postfix) {
+
+			}
+			var editor = new Markdown.Editor(converter);
+			editor.run();
 		}
 	};
 });
