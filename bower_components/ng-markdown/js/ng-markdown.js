2e4bf3dc0353efaea95252741b7ad2f23d7a9957
Balthazar Gronon <bgronon@gmail.com>
2014-04-20 15:57:07 +0200

    revision

--- bower_components/ng-markdown/js/ng-markdown.js
+++ bower_components/ng-markdown/js/ng-markdown.js
@@ -7,7 +7,7 @@
     .directive('ngMarkdown', function ($timeout) {
 
         return {
-            template: '<textarea ng-model="content" msd-elastic></textarea>',
+            template: '<textarea ng-model="content" msd-elastic="\\\n\\\n\\\n"></textarea>',
             restrict: 'E',
             replace: true,
             scope: {
