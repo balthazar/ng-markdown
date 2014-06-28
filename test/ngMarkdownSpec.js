9195e59bf21e4ac08633efb6557df8ec784c5f40
Balthazar Gronon <bgronon@gmail.com>
2014-06-28 21:11:01 +0200

    [Warn] testint tests

--- test/ngMarkdownSpec.js
+++ test/ngMarkdownSpec.js
@@ -3,9 +3,11 @@
 describe('ngMarkdown Directive', function () {
 
 	var body,
+		window,
 		html,
+		el,
 		$scope,
-		bind;
+		$compile;
 
 	var getHtml = function () {
 		return angular.element([
@@ -22,17 +24,48 @@ describe('ngMarkdown Directive', function () {
 	};
 
 	beforeEach(module('ngMarkdown'));
+	beforeEach(inject(function (_$rootScope_, _$compile_, $window) {
+		$scope = _$rootScope_;
+		$compile = _$compile_;
+		window = $window;
+	}));
 
+	var compile = function () {
+		window.document.body.append(getHtml());
+		var el = $compile(window)($scope);
+		$scope.$digest();
+		return el;
+	}
+
+	it('should work as an element', function () {
+		el = compile();
+		expect(el).toBe('<p><em>hi</em></p>');
+	});
+
+	/*
+	it('should work correctly with default model values', function () {
+
+		$scope.bind = 'ok';
+
+		var mark = compile(getHtml());
+		expect(mark).toBe('<p>ok</p>');
+
+		$scope.bind = 'no';
+		$scope.$digest();
+		expect(mark).toBe('<p>ok</p>');
+	});
+*/
+/*
 	beforeEach(inject(function ($rootScope, $compile, $document) {
 
-		$scope = $rootScope.$new();
+		$scope = $rootScope;
 		$scope.bind = '**strongText**';
 
 		body = angular.element($document[0].body);
 		html = getHtml();
 		body.append(html);
 
-		$compile($document)($scope);
+		$compile(html)($scope);
 		$scope.$digest();
 
 	}));
@@ -50,6 +83,7 @@ describe('ngMarkdown Directive', function () {
 	});
 
 	it('should be strong', function () {
+		$scope.bind = '**strongText**';
 		expect($scope.bind).toBe('**strongText**');
 		expect(getPreview().innerHTML).toBe('<p><strong>strongText</strong></p>')
 	});
@@ -63,5 +97,5 @@ describe('ngMarkdown Directive', function () {
 		$scope.$digest();
 		expect(getPreview().innerHTML).toBe('<p><em>italic</em><p>');
 	});
-
+*/
 });
