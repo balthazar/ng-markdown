dbb5f45c5826afcc6c5cd0d0329dfb3667099f28
Aperçu <bgronon@gmail.com>
2014-06-29 16:02:29 +0200

    test(markdown): adding some test for basic markup
    Adding tests about the markup of the following components:
    -code blocks
    -image
    -numbered list
    -simple child lists
    -headings
    -separator

--- test/ngMarkdownSpec.js
+++ test/ngMarkdownSpec.js
@@ -92,4 +92,41 @@ describe('ngMarkdown Directive', function () {
 		expect(getPreview().innerHTML).toBe('<blockquote>\n  <p>The true sign of intelligence is not knowledge but imagination.</p>\n  \n  <p><strong>Albert Einstein</strong></p>\n</blockquote>')
 	}));
 
+	it('should create use some code block properly', inject(function ($timeout) {
+		$scope.bind = 'A basic use of the `$timeout` **Angular** service :\n\n    $timeout(function () {\n        editor.refresh();\n    });';
+		$timeout.flush();
+		expect(getPreview().innerHTML.length).toBe(157);
+		expect(getPreview().innerHTML).toBe('<p>A basic use of the <code>$timeout</code> <strong>Angular</strong> service :</p>\n\n<pre><code>$timeout(function () {\n    editor.refresh();\n});\n</code></pre>')
+	}));
+
+	it('should add an image', inject(function ($timeout) {
+		$scope.bind = '![meow][1]\n\n[1]: https://avatars0.githubusercontent.com/u/6033345';
+		$timeout.flush();
+		expect(getPreview().innerHTML).toBe('<p><img src="https://avatars0.githubusercontent.com/u/6033345" alt="meow" title=""></p>')
+	}));
+
+	it('should create a numbered list', inject(function ($timeout) {
+		$scope.bind = ' 1. This is a numbered list\n 2. just here to make a test about it\n 3. And because I want to !';
+		$timeout.flush();
+		expect(getPreview().innerHTML).toBe('<ol>\n<li>This is a numbered list</li>\n<li>just here to make a test about it</li>\n<li>And because I want to !</li>\n</ol>')
+	}));
+
+	it('should create a list with childs', inject(function ($timeout) {
+		$scope.bind = ' - This is a simple list\n     - but not at easy finally\n          - cause you can make some childs';
+		$timeout.flush();
+		expect(getPreview().innerHTML).toBe('<ul>\n<li>This is a simple list\n<ul><li>but not at easy finally\n<ul><li>cause you can make some childs</li></ul></li></ul></li>\n</ul>')
+	}));
+
+	it('should test the headings', inject(function ($timeout) {
+		$scope.bind = 'This is the h2 heading\n------\n\n## Like this one ##\n\nAnd the h1\n=======\n\n# Also with two writings';
+		$timeout.flush();
+		expect(getPreview().innerHTML).toBe('<h2>This is the h2 heading</h2>\n\n<h2>Like this one</h2>\n\n<h1>And the h1</h1>\n\n<h1>Also with two writings</h1>')
+	}));
+
+	it('should make a separator', inject(function ($timeout) {
+		$scope.bind = 'Hey Hey, do you want to be separated ?\n\n----------\n\nYep';
+		$timeout.flush();
+		expect(getPreview().innerHTML).toBe('<p>Hey Hey, do you want to be separated ?</p>\n\n<hr>\n\n<p>Yep</p>')
+	}));
+
 });
