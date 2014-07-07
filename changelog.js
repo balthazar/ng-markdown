02531c18d3066a18c3b4a5dfcdbf8c226839bc46
Aperçu <bgronon@gmail.com>
2014-07-08 01:55:58 +0200

    docs(core): updating changelogs

--- changelog.js
+++ changelog.js
@@ -6,7 +6,8 @@ var changelog = require('conventional-changelog');
 
 changelog({
 	version: pkg.version,
-	repository: 'https://github.com/Apercu/ng-markdown'
+	repository: 'https://github.com/Apercu/ng-markdown',
+	from: '2.0.0'
 }, function(err, log) {
 	if (err) throw new Error(err);
 	fs.writeFileSync('CHANGELOG.md', log);
