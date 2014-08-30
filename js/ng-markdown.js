cd5d3d7ed473dd5a46487551b7d635a161af0c07
Balthazar Gronon <bgronon@gmail.com>
2014-08-30 17:49:21 +0200

    fix(tests): unit tests are now really operational.
    New indent style with two spaces instead of one tab.
    
    Closes #15.

--- js/ng-markdown.js
+++ js/ng-markdown.js
@@ -1,129 +1,131 @@
 (function () {
-	'use strict';
-	/* global angular */
-	/* global Markdown */
+  'use strict';
+  /* global angular */
+  /* global Markdown */
 
-	angular.module('ngMarkdown', ['monospaced.elastic'])
-	.config(['msdElasticConfig', function (config) {
-		config.append = '\n\n';
-	}])
-	.directive('ngMarkdown', function ($timeout, $compile) {
+  angular.module('ngMarkdown', ['monospaced.elastic'])
+  .config(['msdElasticConfig', function (config) {
+    config.append = '\n\n';
+  }])
+  .directive('ngMarkdown', function ($timeout, $compile) {
 
-		return {
-			template: '<textarea ng-keydown="refresh()"></textarea>',
-			restrict: 'EA',
-			replace: true,
-			scope: {
-				preConversion: '=',
-				postConversion: '=',
-				postNormalization: '=',
-				plainLinkText: '=',
-				preBlockGamut: '=',
-				postBlockGamut: '=',
-				preSpanGamut: '=',
-				postSpanGamut: '=',
-				onPreviewRefresh: '=',
-				helpHandler: '=',
-				customStrings: '=',
-				ngModel: '='
-			},
-			compile: function () {
-				return {
-					post: function (scope, element, attrs) {
+    return {
+      template: '<textarea ng-keydown="refresh()"></textarea>',
+      restrict: 'EA',
+      replace: true,
+      scope: {
+        preConversion: '=',
+        postConversion: '=',
+        postNormalization: '=',
+        plainLinkText: '=',
+        preBlockGamut: '=',
+        postBlockGamut: '=',
+        preSpanGamut: '=',
+        postSpanGamut: '=',
+        onPreviewRefresh: '=',
+        helpHandler: '=',
+        customStrings: '=',
+        ngModel: '='
+      },
+      compile: function () {
+        return {
+          post: function (scope, element, attrs) {
 
-						//Change elasticity, recompile element
-						if (attrs.elastic !== 'false') {
-							element.attr('msd-elastic', '');
-							$compile(element)(scope);
-						}
+            //Change elasticity, recompile element
+            if (attrs.elastic !== 'false') {
+              element.attr('msd-elastic', '');
+              $compile(element)(scope);
+            }
 
-						//Setting defaults params
-						var converter = Markdown.getSanitizingConverter();
-						var editor;
-						var preview;
-						var postfix = '';
-						var prefix = 'wmd-';
-						var helpHandler = null;
-						var strings = {};
+            //Setting defaults params
+            var converter = Markdown.getSanitizingConverter();
+            var editor;
+            var preview;
+            var postfix = '';
+            var prefix = 'wmd-';
+            var helpHandler = null;
+            var strings = {};
 
-						//Test attributes
-						if (attrs.sanitized === 'false') {
-							converter = new Markdown.Converter();
-						}
-						if (attrs.suffix) {
-							postfix = attrs.suffix;
-						}
-						if (attrs.prefix) {
-							prefix = attrs.prefix;
-						}
-						if (attrs.preConversion) {
-							converter.hooks.chain('preConversion', scope.preConversion);
-						}
-						if (attrs.postConversion) {
-							converter.hooks.chain('postConversion', scope.postConversion);
-						}
-						if (attrs.postNormalization) {
-							converter.hooks.chain('postNormalization', scope.postNormalization);
-						}
-						if (attrs.plainLinkText) {
-							converter.hooks.chain('plainLinkText', scope.plainLinkText);
-						}
-						if (attrs.preBlockGamut) {
-							converter.hooks.chain('preBlockGamut', scope.preBlockGamut);
-						}
-						if (attrs.postBlockGamut) {
-							converter.hooks.chain('postBlockGamut', scope.postBlockGamut);
-						}
-						if (attrs.preSpanGamut) {
-							converter.hooks.chain('preSpanGamut', scope.preSpanGamut);
-						}
-						if (attrs.postSpanGamut) {
-							converter.hooks.chain('postSpanGamut', scope.postSpanGamut);
-						}
-						if (attrs.onPreviewRefresh) {
-							editor.hooks.chain('onPreviewRefresh', scope.onPreviewRefresh);
-						}
-						if (attrs.postBlockquoteCreation) {
-							editor.hooks.chain('postBlockquoteCreation', scope.postBlockquoteCreation);
-						}
-						if (attrs.insertImageDialog) {
-							editor.hooks.set('insertImageDialog', scope.insertImageDialog);
-						}
-						if (attrs.helpHandler) {
-							helpHandler = scope.helpHandler;
-						}
-						if (attrs.customStrings) {
-							strings = scope.customStrings;
-						}
+            //Test attributes
+            if (attrs.sanitized === 'false') {
+              converter = new Markdown.Converter();
+            }
+            if (attrs.suffix) {
+              postfix = attrs.suffix;
+            }
+            if (attrs.prefix) {
+              prefix = attrs.prefix;
+            }
+            if (attrs.preConversion) {
+              converter.hooks.chain('preConversion', scope.preConversion);
+            }
+            if (attrs.postConversion) {
+              converter.hooks.chain('postConversion', scope.postConversion);
+            }
+            if (attrs.postNormalization) {
+              converter.hooks.chain('postNormalization', scope.postNormalization);
+            }
+            if (attrs.plainLinkText) {
+              converter.hooks.chain('plainLinkText', scope.plainLinkText);
+            }
+            if (attrs.preBlockGamut) {
+              converter.hooks.chain('preBlockGamut', scope.preBlockGamut);
+            }
+            if (attrs.postBlockGamut) {
+              converter.hooks.chain('postBlockGamut', scope.postBlockGamut);
+            }
+            if (attrs.preSpanGamut) {
+              converter.hooks.chain('preSpanGamut', scope.preSpanGamut);
+            }
+            if (attrs.postSpanGamut) {
+              converter.hooks.chain('postSpanGamut', scope.postSpanGamut);
+            }
+            if (attrs.onPreviewRefresh) {
+              editor.hooks.chain('onPreviewRefresh', scope.onPreviewRefresh);
+            }
+            if (attrs.postBlockquoteCreation) {
+              editor.hooks.chain('postBlockquoteCreation', scope.postBlockquoteCreation);
+            }
+            if (attrs.insertImageDialog) {
+              editor.hooks.set('insertImageDialog', scope.insertImageDialog);
+            }
+            if (attrs.helpHandler) {
+              helpHandler = scope.helpHandler;
+            }
+            if (attrs.customStrings) {
+              strings = scope.customStrings;
+            }
 
-						preview = angular.element(document.querySelector('.' + prefix + 'preview' + postfix));
+            preview = angular.element(document.querySelector('.' + prefix + 'preview' + postfix));
 
-						element.addClass(prefix + 'input' + postfix);
+            element.addClass(prefix + 'input' + postfix);
 
-						editor = new Markdown.Editor(converter, postfix, prefix, helpHandler, strings);
-						editor.run();
+            editor = new Markdown.Editor(converter, postfix, prefix, helpHandler, strings);
+            editor.run();
 
-						scope.refresh = function () {
-							if (attrs.listTransform === 'true') {
-								scope.ngModel = scope.ngModel.replace(/•/g, '-');
-							}
-							$timeout(function () {
-								editor.refreshPreview();
-								angular.forEach(preview.find('code'), function (block) {
-									hljs.highlightBlock(block);
-								});
-							});
-						};
+            scope.refresh = function () {
+              if (attrs.listTransform === 'true') {
+                scope.ngModel = scope.ngModel.replace(/•/g, '-');
+              }
+              $timeout(function () {
+                editor.refreshPreview();
+                angular.forEach(preview.find('code'), function (block) {
+                  hljs.highlightBlock(block);
+                });
+              });
+            };
 
-						scope.$on('refreshMarkdown', function (event, message) {
-							if (!message || message === '' || message === (prefix + postfix)) {
-								scope.refresh();
-							}
-						});
+            scope.$on('refreshMarkdown', function (event, message) {
+              if (!message || message === '' || message === (prefix + postfix)) {
+                scope.refresh();
+              }
+            });
 
-					}
-				};
-			}
-		};
-	});
+            scope.refresh();
+
+          }
+        };
+      }
+    };
+  });
 })();
--- test/unit/ngMarkdown.spec.js
+++ test/unit/ngMarkdown.spec.js
@@ -2,148 +2,131 @@
 
 describe('ngMarkdown Directive', function () {
 
-	var $scope,
-		$compile,
-		elem;
+  var $scope,
+      timeout,
+      elem,
+      template;
 
-	var getPreview = function () {
-		return document.querySelector('.wmd-preview');
-	};
+  beforeEach(module('ngMarkdown'));
 
-	var clearContent = function () {
-		var content = document.querySelector('content');
-		content.parentNode.removeChild(content);
-	};
+  beforeEach(inject(function ($rootScope, $compile, $document, $timeout) {
+    $scope = $rootScope;
+    timeout = $timeout;
 
-	beforeEach(module('ngMarkdown'));
+    var template = angular.element([
+      '<content>',
+        '<div class="wmd-button-bar"></div>',
+        '<ng-markdown ng-model="bind"></ng-markdown>',
+        '<div class="wmd-preview">initialize</div>',
+      '</content>'
+    ].join(''));
 
-	//need to implement this
-	//beforeEach(module('tpl/default.html'));
+    var body = angular.element($document[0].body);
+    body.append(template);
 
-	beforeEach(inject(function ($rootScope, $compile, $document) {
-		$scope = $rootScope;
-		$compile = $compile;
+    elem = $compile(template)($scope);
 
-		elem = angular.element([
-			'<content>',
-			'<div class="wmd-button-bar"></div>',
-			'<ng-markdown ng-model="bind"></ng-markdown>',
-			'<div class="wmd-preview">initialize</div>',
-			'</content>'
-		].join(''));
+    body.append(elem);
+    $scope.$digest();
+  }));
 
-		var body = angular.element($document[0].body);
-		body.append(elem);
+  afterEach(inject(function ($document) {
+    clearContent();
+  }));
 
-		$compile(elem)($scope);
-	}));
+  function getPreview () {
+    return document.querySelector('.wmd-preview');
+  }
 
-	afterEach(inject(function ($document) {
-		clearContent();
-	}));
+  function clearContent () {
+    var content = document.querySelector('content');
+    content.parentNode.removeChild(content);
+  }
 
-	it('should equal 42', function () {
-		expect(42).toBe(42);
-	});
+  function newValue (value) {
+    $scope.bind = value;
+    timeout.flush();
+  }
 
-	it('should attach buttons to the toolbar', function () {
-		expect(document.querySelectorAll('.wmd-button-bar div i').length).toBe(15);
-	});
+  function checkPreview (value) {
+    expect(getPreview().innerHTML).toBe(value);
+  }
 
-	it('should be italic', inject(function ($timeout) {
-		$scope.bind = '*italic*';
-		expect($scope.bind).toBe('*italic*');
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><em>italic</em></p>');
-		});
+  it('should equal 42', function () {
+    newValue(42);
+    expect(42).toBe(42);
+    checkPreview('<p>42</p>');
+  });
 
-		$scope.bind = '**no**';
-		expect($scope.bind).toBe('**no**');
-		$timeout(function () {
-			expect(getPreview().innerHTML).not.toBe('<p><em>italic</em></p>');
-		});
-	}));
+  it('should attach buttons to the toolbar', function () {
+    expect(document.querySelectorAll('.wmd-button-bar div i').length).toBe(15);
+  });
 
-	it('should be strong', inject(function ($timeout) {
-		$scope.bind = '**strongText**';
-		expect($scope.bind).toBe('**strongText**');
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><strong>strongText</strong></p>')
-		});
-	}));
+  it('should be italic', function () {
+    newValue('*italic*');
+    expect($scope.bind).toBe('*italic*');
+    checkPreview('<p><em>italic</em></p>');
+  });
 
-	it('should be strong and italic', inject(function ($timeout) {
-		$scope.bind = '***strong and italic text***';
-		expect($scope.bind).toBe('***strong and italic text***');
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><em><strong>strong and italic text</strong></em></p>')
-		});
-	}));
+  it('should be strong', function () {
+    newValue('**strongText**');
+    expect($scope.bind).toBe('**strongText**');
+    checkPreview('<p><strong>strongText</strong></p>');
+  });
 
-	it('should be strong and italic', inject(function ($timeout) {
-		$scope.bind = '***strong and italic text***';
-		expect($scope.bind).toBe('***strong and italic text***');
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><em><strong>strong and italic text</strong></em></p>')
-		});
-	}));
+  it('should be strong and italic', function () {
+    newValue('***strong and italic text***');
+    expect($scope.bind).toBe('***strong and italic text***');
+    checkPreview('<p><em><strong>strong and italic text</strong></em></p>');
+  });
 
-	it('should add a link', inject(function ($timeout) {
-		$scope.bind = '[My StackOverflow][1]\n\n[1]: http://stackoverflow.com/users/2054072/aper%C3%A7u';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><a href="http://stackoverflow.com/users/2054072/aper%C3%A7u">My StackOverflow</a></p>')
-		});
-	}));
+  it('should be strong and italic', function () {
+    newValue('***strong and italic text***');
+    expect($scope.bind).toBe('***strong and italic text***');
+    checkPreview('<p><em><strong>strong and italic text</strong></em></p>');
+  });
 
-	it('should create a citation block', inject(function ($timeout) {
-		$scope.bind = '> The true sign of intelligence is not knowledge but imagination.\n>\n> **Albert Einstein**';
-		$timeout(function () {
-			expect(getPreview().innerHTML.length).toBe(144);
-			expect(getPreview().innerHTML).toBe('<blockquote>\n  <p>The true sign of intelligence is not knowledge but imagination.</p>\n  \n  <p><strong>Albert Einstein</strong></p>\n</blockquote>')
-		});
-	}));
+  it('should add a link', function () {
+    newValue('[My StackOverflow][1]\n\n[1]: http://stackoverflow.com/users/2054072/aper%C3%A7u');
+    checkPreview('<p><a href="http://stackoverflow.com/users/2054072/aper%C3%A7u">My StackOverflow</a></p>');
+  });
 
-	it('should create use some code block properly', inject(function ($timeout) {
-		$scope.bind = 'A basic use of the `$timeout` **Angular** service :\n\n    $timeout(function () {\n        editor.refresh();\n    });';
-		$timeout(function () {
-			expect(getPreview().innerHTML.length).toBe(157);
-			expect(getPreview().innerHTML).toBe('<p>A basic use of the <code>$timeout</code> <strong>Angular</strong> service :</p>\n\n<pre><code>$timeout(function () {\n    editor.refresh();\n});\n</code></pre>')
-		});
-	}));
+  it('should create a citation block', function () {
+    newValue('> The true sign of intelligence is not knowledge but imagination.\n>\n> **Albert Einstein**');
+    expect(getPreview().innerHTML.length).toBe(144);
+    checkPreview('<blockquote>\n  <p>The true sign of intelligence is not knowledge but imagination.</p>\n  \n  <p><strong>Albert Einstein</strong></p>\n</blockquote>');
+  });
 
-	it('should add an image', inject(function ($timeout) {
-		$scope.bind = '![meow][1]\n\n[1]: https://avatars0.githubusercontent.com/u/6033345';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p><img src="https://avatars0.githubusercontent.com/u/6033345" alt="meow" title=""></p>')
-		});
-	}));
+  it('should create use some code block properly', function () {
+    //Take care of the new highlight integration
+    newValue('A basic use of the `$timeout` **Angular** service :\n\n    $timeout(function () {\n        editor.refresh();\n    });');
+    expect(getPreview().innerHTML.length).toBe(366);
+    checkPreview('<p>A basic use of the <code class=" hljs bash"><span class="hljs-variable">$timeout</span></code> <strong>Angular</strong> service :</p>\n\n<pre><code class=" hljs php"><span class="hljs-variable">$timeout</span>(<span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-params">()</span> </span>{\n    editor.refresh();\n});\n</code></pre>');
+  });
 
-	it('should create a numbered list', inject(function ($timeout) {
-		$scope.bind = ' 1. This is a numbered list\n 2. just here to make a test about it\n 3. And because I want to !';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<ol>\n<li>This is a numbered list</li>\n<li>just here to make a test about it</li>\n<li>And because I want to !</li>\n</ol>')
-		});
-	}));
+  it('should add an image', function () {
+    newValue('![meow][1]\n\n[1]: https://avatars0.githubusercontent.com/u/6033345');
+    checkPreview('<p><img src="https://avatars0.githubusercontent.com/u/6033345" alt="meow" title=""></p>');
+  });
 
-	it('should create a list with childs', inject(function ($timeout) {
-		$scope.bind = ' - This is a simple list\n     - but not at easy finally\n          - cause you can make some childs';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<ul>\n<li>This is a simple list\n<ul><li>but not at easy finally\n<ul><li>cause you can make some childs</li></ul></li></ul></li>\n</ul>')
-		});
-	}));
+  it('should create a numbered list', function () {
+    newValue(' 1. This is a numbered list\n 2. just here to make a test about it\n 3. And because I want to !');
+    checkPreview('<ol>\n<li>This is a numbered list</li>\n<li>just here to make a test about it</li>\n<li>And because I want to !</li>\n</ol>');
+  });
 
-	it('should test the headings', inject(function ($timeout) {
-		$scope.bind = 'This is the h2 heading\n------\n\n## Like this one ##\n\nAnd the h1\n=======\n\n# Also with two writings';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<h2>This is the h2 heading</h2>\n\n<h2>Like this one</h2>\n\n<h1>And the h1</h1>\n\n<h1>Also with two writings</h1>')
-		});
-	}));
+  it('should create a list with childs', function () {
+    newValue(' - This is a simple list\n     - but not at easy finally\n          - cause you can make some childs');
+    checkPreview('<ul>\n<li>This is a simple list\n<ul><li>but not at easy finally\n<ul><li>cause you can make some childs</li></ul></li></ul></li>\n</ul>');
+  });
 
-	it('should make a separator', inject(function ($timeout) {
-		$scope.bind = 'Hey Hey, do you want to be separated ?\n\n----------\n\nYep';
-		$timeout(function () {
-			expect(getPreview().innerHTML).toBe('<p>Hey Hey, do you want to be separated ?</p>\n\n<hr>\n\n<p>Yep</p>')
-		});
-	}));
+  it('should test the headings', function () {
+    newValue('This is the h2 heading\n------\n\n## Like this one ##\n\nAnd the h1\n=======\n\n# Also with two writings');
+    checkPreview('<h2>This is the h2 heading</h2>\n\n<h2>Like this one</h2>\n\n<h1>And the h1</h1>\n\n<h1>Also with two writings</h1>');
+  });
+
+  it('should make a separator', function () {
+    newValue('Hey Hey, do you want to be separated ?\n\n----------\n\nYep');
+    checkPreview('<p>Hey Hey, do you want to be separated ?</p>\n\n<hr>\n\n<p>Yep</p>');
+  });
 
 });
